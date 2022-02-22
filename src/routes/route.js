2ee78366ecf99af7f954bfdcf91ceff58314c7fb
Amit01k <amitkumar1437786@gmail.com>
2022-02-22 15:37:32 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,16 @@
+let Obj=require('..loggerfolder/logger.js')
+
+
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/test-me', function (req, res) {
+    obj.abc('thorium')
+    obj.welcome();
+    console.log(obj.url)
     res.send('My first ever api!')
 });
+
 
 module.exports = router;
---
+++ src/validator/formatter.js
@@ -0,0 +1,14 @@
+var myString="   amit  "
+var trimStr=myString.trim();
+console.log(trimStr)
+ 
+
+
+var str="FUNCTION ASSIGNMENT"
+var lowerstr=str.toLocaleLowerCase();
+console.log(lowerstr)
+
+
+var strupp="this assignment is given by functionup"
+var uppstr=strupp.toUpperCase();
+console.log(uppstr)
