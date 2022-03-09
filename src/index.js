0944143dfd827b998ced0b1c71b92ed6e383fb68
Amit01k <amitkumar1437786@gmail.com>
2022-03-09 23:29:52 +0530

    authorise_midd_UT_same

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Amit:THmzVZv3QBLW11Z1@cluster0.zkmhi.mongodb.net/Amit-middleware02", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,45 @@
+const jwt = require("jsonwebtoken");
+
+
 const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
+    let token=req.headers["x-auth-token"]
+    if(token==undefined){
+        console.log("done")
+    
 
     next()
+    }
+    else{
+        console.log("requst is missing madatory header")
+    }
 }
 
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
+    let token = req.headers["x-auth-token"]
+    if(!token) return res.send({status: false, msg: "token must be present in the request header"})
+    let decodedToken = jwt.verify(token, 'functionup-thorium')
+
+    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
+
+    let userModify=req.params.userId
+    let userLogger=decodedToken.userId
+    if(userModify!=userLogger)
+    return res.send({status:false,msg:"user not authorized"})
+
+
+
+    // let xauth=req.headers["x-auth-token"]
+    // if(!xauth) return res.send("token must be required")
+    
+    // let decode=jwt.verify(token,"functionup-thorium")
+    // if(!decode) return res.send("usertoken is invalid")
+
     next()
 }
+
+module.exports.authenticate=authenticate
+module.exports.authorise=authorise
